27e6f5eec3d290be2bb74ad08a47cbb7e9a01960
MarcosViniciusPinho <marcosjava2008@gmail.com>
2017-10-01 18:19:52 -0300

    Adicionando mensagens de erro de negócio na aplicação.

--- apiUi/assets/js/controllers/usuarioController.js
+++ apiUi/assets/js/controllers/usuarioController.js
@@ -19,7 +19,7 @@ function UsuarioController(usuarioService, $location, $routeParams, toastr) {
 
     vm.save = function () {
         usuarioService.saveOrUpdate(vm.usuario)
-            .then(saveSucess).catch(saveFailed);
+            .then(saveSucess).catch(throwException);
     };
 
     vm.back = function(){
@@ -28,13 +28,13 @@ function UsuarioController(usuarioService, $location, $routeParams, toastr) {
 
     function listar() {
         usuarioService.findAll()
-            .then(findAllSucess).catch(findAllFailed);
+            .then(findAllSucess).catch(throwException);
     }
 
     function carregarUsuario(){
         if($routeParams != undefined && $routeParams.id != undefined){
             usuarioService.getUsuario($routeParams.id)
-                .then(getUsuarioSucess).catch(getUsuarioFailed);
+                .then(getUsuarioSucess).catch(throwException);
         }
     }
 
@@ -46,28 +46,24 @@ function UsuarioController(usuarioService, $location, $routeParams, toastr) {
         vm.usuarios = response.data;
     }
 
-    function findAllFailed(response) {
-        throwException(response);
-    }
-
     function saveSucess() {
         toastr.success('Operação realizada com sucesso!');
         returnToOrigin();
-    }
-
-    function saveFailed(response) {
-        throwException(response);
     }
 
     function getUsuarioSucess(response) {
         vm.usuario = response.data;
     }
 
-    function getUsuarioFailed(response) {
-        throwException(response);
+    function throwException(response){
+        if(response.data == null){
+            toastr.error('Houve uma falha na comunicação com o recurso da API - Status: ' + response.xhrStatus);
+        } else{
+            var listagemDeErros = response.data.messagesBusiness;
+            for(var i = 0; i < listagemDeErros.length; i++){
+                toastr.error(listagemDeErros[i]);
+            }
+        }
     }
 
-    function throwException(response){
-        toastr.error('Houve uma falha na comunicação com o recurso da API - Status: ' + response.xhrStatus);
-    }
 }
